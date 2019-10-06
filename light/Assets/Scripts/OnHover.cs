2cd6f3bc91f5cc66613487c14e2a47554eba0f44
PC Portable Hugo <hugo.raguin@gmail.com>
2019-10-06 18:18:59 +0200

    Menu

---
+++ light/Assets/Scripts/OnHover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class OnHover : MonoBehaviour, IPointerEnterHandler
+{
+    public UnityEvent OnHoverEvent;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        OnHoverEvent?.Invoke();
+    }
+}
---
+++ light/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public AudioSource source;
+    public SoundAsset audio;
+
+    public virtual void Play() {
+        source.clip = audio.clip;
+        source.volume = audio.volume;
+        source.pitch = audio.pitch;
+        source.PlayOneShot(audio.clip);
+    }
+}
